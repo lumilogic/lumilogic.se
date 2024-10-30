c5496c5d27757dccb5a863ce26a65593275b9092
Samuel Scheidegger <samuel@scheidegger.se>
2024-10-31 00:15:53 +0100

    switch to browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './views/Home/Home';
 import Contact from './views/Contact/Contact';
@@ -10,14 +8,14 @@ import Research from './views/Research/Research';
 import Footer from './components/Footer/Footer';
 import Climatebug from './views/Products/Climatebug';
 import SustainSight from './views/Products/SustainSight';
+import ScrollToTop from './components/ScrollToTop'; // import the component
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
+      <ScrollToTop /> {/* Add here to ensure scroll reset on each route change */}
       <div className="min-vh-100 d-flex flex-column">
-
         <Navbar />
-        {/* <div className="container flex-grow-1"> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
@@ -25,12 +23,7 @@ function App() {
           <Route path="/climatebug" element={<Climatebug />} />
           <Route path="/sustainsight" element={<SustainSight />} />
           <Route path="/research" element={<Research />} />
-          {/* <Route path="/home" element={<Home />} />
-            
-            <Route path="/team" element={<Team />} /> */}
-          {/* Other routes can be added here */}
         </Routes>
-        {/* </div> */}
         <Footer />
       </div>
     </Router>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,77 +1,51 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.scss';
-import logo_small from '@assets/images/logo_small.png'; // Import the logo image
+import logo_small from '@assets/images/logo_small.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
-import climateBugLogo from '@assets/images/climatebug.png'; // Import another image from the paper
+import climateBugLogo from '@assets/images/climatebug.png';
 
 function Navbar() {
     const [scrolled, setScrolled] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
-            const offset = window.scrollY;
-            if (offset > 50) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
+            setScrolled(window.scrollY > 50);
         };
 
         window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
+        return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
     return (
         <nav className={`navbar navbar-expand-lg fixed-top ${scrolled ? 'scrolled' : ''}`}>
-
-
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    <img src={logo_small} alt="Lumilogic logo" className="navbar-logo" />Lumilogic
-                </a>
+                <Link className="navbar-brand" to="/">
+                    <img src={logo_small} alt="Lumilogic logo" className="navbar-logo" /> Lumilogic
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        {/* <li className="nav-item">
-                            <a className="nav-link" href="/">Home</a>
-                        </li> */}
-
-                        {/* <li className="nav-item">
-                            <a className="nav-link" href="/#sustainsight">Products</a>
-                        </li> */}
-
                         <li className="nav-item dropdown">
-
                             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Products
                             </a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#sustainsight">SustainSight</a></li>
-                                <li><a className="dropdown-item" href="#climatebug">climateBUG <img src={climateBugLogo} height="25pt"></img></a></li>
-
+                                <li><Link className="dropdown-item" to="/sustainsight">SustainSight</Link></li>
+                                <li><Link className="dropdown-item" to="/climatebug">climateBUG <img src={climateBugLogo} height="25pt" alt="ClimateBug logo" /></Link></li>
                             </ul>
-
                         </li>
-
                         <li className="nav-item">
-                            <a className="nav-link" href="/#research">Research</a>
+                            <Link className="nav-link" to="/research">Research</Link>
                         </li>
-
                         <li className="nav-item">
-                            <a className="nav-link" href="/#team">Team</a>
+                            <Link className="nav-link" to="/team">Team</Link>
                         </li>
-
-
-                        {/* <li className="nav-item">
-                            <a className="nav-link" target="_blank" href="https://www.climatebug.se/">climateBUG <FontAwesomeIcon icon={faExternalLinkAlt} /></a>
-                        </li> */}
                         <li className="nav-item">
-                            <a className="nav-link" href="/#contact">Contact</a>
+                            <Link className="nav-link" to="/contact">Contact</Link>
                         </li>
                     </ul>
                 </div>
